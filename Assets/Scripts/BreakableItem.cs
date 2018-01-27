b0c177864c4be79bd660dd3f013b5e51cfc1554c
Jerry B Andersen <jerrybandersen@yahoo.de>
2018-01-27 15:28:07 +0100

    working Breakables and BuyableAreas

---
+++ Assets/Scripts/BreakableItem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableItem : Item, Breakable
+{
+	public float breakForce = .1f;
+	public GameObject fragmentsPrefab = null;
+	public GameObject fragments = null;
+
+	public bool isBroken{
+		get{ 
+			return fragments != null;
+		}
+	}
+
+	void OnCollisionEnter(Collision c){
+		var rel = c.relativeVelocity;
+		var otherrigid = c.collider.GetComponent<Rigidbody> ();
+		Vector3 averageNormal = Vector3.zero;
+		foreach (var con in c.contacts) {
+			averageNormal += con.normal;
+		}
+		averageNormal /= c.contacts.Length;
+
+		var force = (otherrigid?otherrigid.mass:1f)*Vector3.Dot (rel, averageNormal);
+		if (force > breakForce) {
+			Break ();
+		}
+	}
+
+	public void Break(){
+		fragments = Instantiate (fragmentsPrefab, transform,false);
+		fragments.transform.SetParent (null,true);
+		foreach (var ri in fragments.GetComponentsInChildren<Rigidbody>()) {
+			ri.velocity = rigid.velocity;
+		}
+		Destroy (gameObject);
+	}
+}
