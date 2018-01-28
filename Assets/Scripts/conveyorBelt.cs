64236a34e3067a043334c7424e138906c99df604
Franzi93 <fneu93@googlemail.com>
2018-01-28 03:47:32 +0100

    created conveyor belt script

---
+++ Assets/Scripts/conveyorBelt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conveyorBelt : MonoBehaviour {
+
+    List<Rigidbody> rigids = new List<Rigidbody>();
+
+    void OnTriggerEnter(Collider collider) {
+        rigids.Add(collider.GetComponent<Rigidbody>());
+
+    }
+
+    void OnTriggerExit(Collider collider) {
+        rigids.Remove(collider.GetComponent<Rigidbody>());
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < rigids.Count; i++) {
+            rigids[i].AddForce(Vector3.right);
+        }
+    }
+       
+}
