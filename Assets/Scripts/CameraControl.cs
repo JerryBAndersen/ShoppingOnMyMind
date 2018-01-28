e18d792e3bfa9996cbe94c9ddd09d10060ccd5a6
Daniel Harrison <harrisonfx@gmx.net>
2018-01-28 15:18:06 +0100

    FINAL SHIT

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,24 +19,26 @@ public class CameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		float maxDist = 0f;
-		center.position = Vector3.zero;
+        int pl = 0;
+		Vector3 flat = Vector3.zero;
 		foreach (var player in GameController.players) {
 			if (player == null) {
 				continue;
 			}
-			center.position += player.transform.position;
+            flat += player.GetComponentInChildren<SkinnedMeshRenderer>().transform.position;
+            pl++;
 			foreach (var other in GameController.players) {
 				if (other == player || other == null) {
 					continue;
 				}
-				maxDist = Mathf.Max (maxDist,Vector3.Distance(other.transform.position,player.transform.position));
-				center.position += player.transform.position;
+				maxDist = Mathf.Max (maxDist,Vector3.Distance(other.GetComponentInChildren<SkinnedMeshRenderer>().transform.position,player.GetComponentInChildren<SkinnedMeshRenderer>().transform.position));
 			}
 		}
-		Vector3 flat = center.position;
 		flat.y = 0;
-		flat /= GameController.players.Length!=0?GameController.players.Length:1;
+		flat /= pl!=0?pl:1;
+        flat = Vector3.Lerp(flat,center.position, .9f);
 		center.position = flat;
-		camera.transform.localPosition = Vector3.Lerp (lowPoint.localPosition,highPoint.localPosition,maxDist/30f);
+        Vector3 nu = Vector3.Lerp (lowPoint.localPosition,highPoint.localPosition,maxDist/30f);
+		camera.transform.localPosition = Vector3.Lerp(nu,camera.transform.localPosition,.99f);
 	}
 }
